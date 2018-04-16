6b77e0feac58342191116e6e3044cc03e6d05984
MartinMcK <33032026+MartinMcK@users.noreply.github.com>
2018-04-16 23:57:13 +0100

    Initial commit

---
+++ Assets/Scripts/Bullet2D.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Bullet2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float destroyTime = 0.7f;
+    void Start()
+    {
+        Invoke("Die", destroyTime);
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+    void OnDestroy()
+    {
+        CancelInvoke("Die");
+    }
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+    }
+}
